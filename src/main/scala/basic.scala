8dd4335fcecc2ea1c55135e72e316f55dacc607f
Zach Cox <zcox522@gmail.com>
2013-03-13 15:31:49 -0500

    foo

--- src/main/scala/basic.scala
+++ src/main/scala/basic.scala
@@ -10,6 +10,7 @@ The logging actor's supervisor restarts it on the exception
 Things to note:
   - supervisor creates the child
   - child throws the exception, but supervisor receives it & determines what happens to child
+  - child throwing an exception doesn't just bring the entire application/system to a halt
   - supervisor gets the message the child was processing when exception was thrown
   - number sending actor never knows about exception, restarting, new child instance, etc
   - even when using system.actorOf, the actor is still supervised
---
+++ src/main/scala/let-it-crash.scala
@@ -0,0 +1,5 @@
+package iascala
+
+//an actor that doesn't let it crash
+
+//an actor that lets it crash
